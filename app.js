ec643f25a3c9c97982c12e4ea45c0b5f3da0f208
kemmerle <kemmerle@umich.edu>
2019-09-19 17:49:40 -0400

    Got the show pages working for individual book information and got the book form working so that users can post other books

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 
 const db = require('./models');
 const Book = db.Book;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json())
 
 app.use('/static', express.static('public'));
 
@@ -17,6 +22,21 @@ app.get('/books', async (req, res, next) => {
   const bookList = books.map(book => book.toJSON());
 
   res.render('index', { books: bookList });
+});
+
+app.get('/books/new', (req, res) => {
+  res.render('new-book');
+});
+
+app.post('/books/new', async (req, res, next) => {
+  const request = req.body
+  const book = await Book.create(request);
+  res.redirect('/books/' + book.id);
+});
+
+app.get('/books/:id', async (req, res, next) => {
+  const book = await Book.findByPk(req.params.id);
+  res.render('show', { book: book });
 });
 
 app.listen(3000, function() {
